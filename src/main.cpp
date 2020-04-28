b058038941a7d5eb13168d348b8d8fbeef2a0442
André Biseth <andre.biseth@onyxcentersource.com>
2020-04-28 22:05:51 +0200

    Testing out code snippets

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,39 @@
 #include "fifo.c"
 #include "nmea.c"
 
+#define PRINTBIN(Num)                                           \
+  for (uint32_t t = (1UL << (sizeof(Num) * 8) - 1); t; t >>= 1) \
+    Serial.write(Num &t ? '1' : '0'); // Prints a binary number with leading zeros (Automatic Handling)
+#define PRINTBINL(Num)                        \
+  for (int i = 0; i < (sizeof(Num) * 8); i++) \
+    Serial.write(((Num >> i) & 1) == 1 ? '1' : '0'); // Prints a binary number with following Placeholder Zeros  (Automatic Handling)
+
 SoftwareSerial ss(D6, D7); // Here are the aliased pins
 
-void ICACHE_RAM_ATTR rxINT();
+void ICACHE_RAM_ATTR RX_CMX589();
 
-static uint8_t rx_pkt[128];
-static uint8_t pkt_size;
+#define radio_tx_buf_len 850 // Size of radio tx ring buffer
+#define radio_rx_buf_len 300 // Size of radio rx ring buffer
+#define pinRXD_U D1
 
-uint8_t rxstate;
-uint16_t bitcount;
+volatile byte rx_status = 0;               // Status of transmission
+volatile byte status_flags_hi = B00000000; // Stream parameters
+volatile byte status_flags_lo = B00000000;
 
-enum eRxState
-{
-  RX_INIT,
-  RX_PREAMBLE,
-  RX_START,
-  RX_PRELOAD,
-  RX_DATA
-};
+volatile unsigned int time_out_timer;
+volatile byte input_rx_buffer;                           // Temp input buffer from CMX589, 8 bit buffer is filled by RX_589.
+volatile byte input_rx_buffer_bit_counter = 0;           // When 8 bits are received byte full wil be placed in radio_rx_buffer,
+volatile int radio_rx_buffer_counter = radio_rx_buf_len; // pointer for end of buffer indication
+volatile byte radio_tx_buffer[radio_tx_buf_len + 1];     // ringbuffer for datastream from CMX589
+volatile int radio_tx_buffer_in_counter = 0;             // pointer for end of buffer indication
+volatile int radio_tx_buffer_out_counter = 0;
+volatile int radio_tx_buffer_bit_counter = 0;
+volatile int radio_tx_buffer_gen_counter = 0;
+volatile boolean rx_int_flag = false;
+
+volatile byte temp_shift_buffer[23];                            // Fifo buffer for data analisys. 21 X 8 bits can be shift right.
+volatile int temp_shift_buffer_counter = radio_rx_buf_len - 50; // When 8 bits are shifted a new byte pointed by counter will be
+volatile byte temp_shift_buffer_bit_counter = 0;                // placed in temp_shift_buffer[0].
 
 unsigned long vhfMillis = 0;
 bool scan = false;
@@ -42,177 +57,123 @@ void setup()
   ss.print("\r\n");
   ss.readStringUntil('\n');
   ss.print("AT+DMOSETGROUP=1,161.9750,161.9750,0000,0,0000");
+  // ss.print("AT+DMOSETGROUP=1,162.0250,162.0250,0000,0,0000");
   ss.print("\r\n");
   ss.readStringUntil('\n');
 
   pinMode(D4, INPUT); // declare pushbutton as input
   pinMode(D2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(D2), rxINT, RISING);
-  FIFO_Init();
+  attachInterrupt(digitalPinToInterrupt(D2), RX_CMX589, RISING);
+
+  Serial.println("");
+  Serial.println("Searching...");
+}
+
+//========================================= Wait for 24 bit Preamble ===================================================
+void Wait_for_preamble()
+{
+  if (temp_shift_buffer[2] == B10101010 && temp_shift_buffer[1] == B10101010 && temp_shift_buffer[0] == B10101010)
+  {
+    rx_status = 1;
+    Serial.println("Preamble found!");
+  }
+}
+
+//========================================= Wait for Start flag ===================================================
+void Wait_for_start_flag()
+{
+  if (temp_shift_buffer[0] == B01111110)
+  {
+    rx_status = 2;
+    Serial.println("Start flag found!");
+  }
+}
+
+//========================================= Wait for Stop flag ===================================================
+void Wait_for_stop_flag()
+{
+  if (temp_shift_buffer[0] == B01111110)
+  {
+    rx_status = 0;
+    Serial.println("Stop flag found!!");
+  }
+}
+
+//============================ Rotate temp_shift_buffer ===========================================================
+void RX_buffer_shift()
+{
+  temp_shift_buffer_bit_counter += 1; // Increment curent possition
+
+  if (temp_shift_buffer_bit_counter > 7) // Check if byte is full
+  {
+    temp_shift_buffer_bit_counter = 0;                                 // If full clear bit counter
+    temp_shift_buffer[0] = radio_tx_buffer[temp_shift_buffer_counter]; //!       // Read byte from ringbuffer
+    temp_shift_buffer_counter -= 1;                                    // Relocate ringbuffer
+    if (temp_shift_buffer_counter == 0)
+      temp_shift_buffer_counter = radio_rx_buf_len; // Reloop ringbuffer
+  }
+  // RX shift buffer
+  for (int RX_buffer_shift_counter = 22; RX_buffer_shift_counter >= 0; RX_buffer_shift_counter--)
+  {
+    temp_shift_buffer[RX_buffer_shift_counter] = temp_shift_buffer[RX_buffer_shift_counter] >> 1;
+    bitWrite(temp_shift_buffer[RX_buffer_shift_counter], 7, bitRead(temp_shift_buffer[RX_buffer_shift_counter - 1], 0));
+  }
+}
+//========================================= Interrupt routine RX clk from CMX589 =================================
+void RX_CMX589()
+{
+
+  time_out_timer += 1;
+  bitWrite(input_rx_buffer, 7, digitalRead(pinRXD_U)); // Read RX data pin normal
+  input_rx_buffer_bit_counter += 1;                    // Increment bit counter
+
+  if (input_rx_buffer_bit_counter == 8) // If byte is full copy byte to ringbuffer
+  {
+    // PRINTBIN(input_rx_buffer);
+    // Serial.print(" - ");
+    // Serial.println(input_rx_buffer, HEX);
+
+    radio_tx_buffer[radio_rx_buffer_counter] = input_rx_buffer; //!     // Copy byte to ringbuffer
+    radio_rx_buffer_counter -= 1;                               // Rotate ringbuffer
+    if (radio_rx_buffer_counter == 0)
+      radio_rx_buffer_counter = radio_rx_buf_len; // Loop buffer around if needed
+    input_rx_buffer_bit_counter = 0;              //
+  }
+  else
+  {                                         // If byte is not full
+    input_rx_buffer = input_rx_buffer >> 1; // Rotate input buffer
+  }
+
+  rx_int_flag = true; // Set rx_int_flag for general purposes.
 }
 
 void loop()
 {
-  if (Serial.available())
-  {                          // If anything comes in Serial (USB),
-    ss.write(Serial.read()); // read it and send it out Serial1 (pins 0 & 1)
-  }
+  // if (Serial.available())
+  // {                          // If anything comes in Serial (USB),
+  //   ss.write(Serial.read()); // read it and send it out Serial1 (pins 0 & 1)
+  // }
 
-  if (ss.available())
-  {                          // If anything comes in Serial1 (pins 0 & 1)
-    Serial.write(ss.read()); // read it and send it out Serial (USB)
-  }
+  // if (ss.available())
+  // {                          // If anything comes in Serial1 (pins 0 & 1)
+  //   Serial.write(ss.read()); // read it and send it out Serial (USB)
+  // }
 
-  //if (millis() > buttonMillis)
-  { // check if the input is HIGH (button released)
-    buttonMillis = millis() + 1UL;
-
-    pkt_size = FIFO_Get_Packet(rx_pkt, 128); //SIZE OF BUFFER
-
-    // A good packet must be bigger than the 2-byte CRC !
-    if (pkt_size > 2)
-    {
-      // Send NMEA message - discarding the CRC
-      NMEA_Send(rx_pkt, pkt_size - 2);
-    }
-  }
-}
-
-void rxINT()
-{
-  //static local variables retain their value when the function is exited
-  uint8_t dat;
-  static uint8_t lastdat; //removing storage class specifier: static should break functionality
-  static uint8_t dbyte;
-  static uint16_t shiftreg;
-  static uint16_t newbit;
-  static uint16_t crcreg;
-
-  dat = digitalRead(D1);
-
-  // NRZI Decode latest bit and shift into register
-  shiftreg <<= 1;
-  if (dat == lastdat)
-    // Data the same = 1
-    shiftreg |= 0x0001;
-  else
-    // Data changed = 0;
-    shiftreg &= 0xfffe;
-
-  // Save the last state of the data line
-  lastdat = dat;
-
-  // Execute state-machine
-  switch (rxstate)
+  if (rx_int_flag == true)
   {
-  // Set RxDCaqc and PLLacq after an initial delay
-  // Go to the RX_PREAMBLE state
-  case RX_INIT:
-    if (!(--bitcount))
-    {
-      rxstate = RX_PREAMBLE;
-      shiftreg = 0l;
-    }
-    break;
+    RX_buffer_shift(); // Rotate RX buffer
 
-  // Look for 16 bits of preamble
-  case RX_PREAMBLE:
-    if (shiftreg == 0x5555)
-    {
-      bitcount = 0x00;
-      rxstate = RX_START;
-    }
-    break;
+    // Wait for preamble
+    if ((rx_status == 0) && (rx_int_flag == true))
+      Wait_for_preamble();
 
-  // Look for start flag
-  case RX_START:
-    //Serial.println("RX_START");
-    if ((shiftreg & 0x00ff) == 0x007e)
-    {
-      bitcount = 0;
-      rxstate = RX_PRELOAD;
-    }
-    else if (bitcount >= 24)
-    {
-      // Start flag not found - go back and look for
-      // preamble
-      rxstate = RX_PREAMBLE;
-    }
-    break;
+    // Wait for frame sync
+    if ((rx_status == 1) && (rx_int_flag == true))
+      Wait_for_start_flag();
 
-  // Load an initial 8 bits into the shift register.
-  // This means that when we see the end flag we have just
-  // passed the last CRC bit through the CRC calculation
-  case RX_PRELOAD:
-    //Serial.println("RX_PRELOAD");
-    if (++bitcount == 8)
-    {
-      // Go to the receiving state
-      bitcount = 0;
-      crcreg = 0xffff;
-      rxstate = RX_DATA;
-      FIFO_Reset_Packet();
-    }
-    break;
-
-  // Receive data
-  case RX_DATA:
-
-    //Serial.println("RX_DATA");
-    // Remove stuffing bits
-    // An extra zero is inserted after five 1's by the transmitter
-    if ((shiftreg & 0x3f00) != 0x3e00)
-    {
-      // It's not a stuffing bit
-
-      // Increment the bit count
-      ++bitcount;
-      // Extract the new data bit
-      newbit = (shiftreg >> 8) & 0x0001;
-      // Shift new bit into a byte
-      dbyte = (dbyte >> 1) | ((shiftreg >> 1) & 0x0080);
-
-      // If 8 bits received but into FIFO
-      if (!(bitcount & 0x07))
-        FIFO_Put(dbyte);
-
-      // Pass new bit through CRC calculation
-      if ((crcreg ^ newbit) & 0x0001)
-        // Xor with the CRC polynomial (X^16 + X^12 + X^5 + 1)
-        crcreg = (crcreg >> 1) ^ 0x8408;
-      else
-      {
-        // There appears to be a bug in the Keil compiler - right shift
-        // doesn't work unless something else is done on the same line.
-        // Here I xor shifted result with 1 and then xor again to correct
-        // this back
-        crcreg = (crcreg >> 1); // ^ 0x0001;
-        //crcreg ^= 0x0001;
-      }
-    }
-
-    // Have we got an end flag ?
-    if ((shiftreg & 0x00ff) == 0x007e)
-    {
-      //Serial.println("End");
-      // Reset state machine to look for preamble
-      rxstate = RX_PREAMBLE;
-      // Check for good CRC
-      // This should give a result of 0xF0B8
-      if (crcreg == 0xf0b8)
-      {
-        // Good packet received
-        // ... Release into FIFO
-        FIFO_Write_Packet();
-        Serial.printf("%04X\n", crcreg);
-        Serial.printf("\n---%d\n", bitcount);
-      }
-    }
-    break;
-
-  default:
-    break;
+    if ((rx_status == 2) && (rx_int_flag == true))
+      Wait_for_stop_flag();
   }
-  // Clear the GPIO interrupt.
-  //GIFR |= (1 << INTF0); // cleared automatically, alternatively can clear by writing logical 1 to it.
+
+  rx_int_flag = false;
 }
