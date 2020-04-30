0da24ebc70a5abbb646b2d2e78395584a3b418fa
André Biseth <andre.biseth@onyxcentersource.com>
2020-04-30 20:54:54 +0200

    Testing buffer approaches

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,22 @@ SoftwareSerial ss(D6, D7); // Here are the aliased pins
 
 void ICACHE_RAM_ATTR RX_CMX589();
 
-#define radio_tx_buf_len 850 // Size of radio tx ring buffer
-#define radio_rx_buf_len 300 // Size of radio rx ring buffer
+#define ais_rx_buf_len 32 // Size of radio rx ring buffer
 #define pinRXD_U D1
 
-volatile byte rx_status = 0;               // Status of transmission
-volatile byte status_flags_hi = B00000000; // Stream parameters
-volatile byte status_flags_lo = B00000000;
+volatile byte rx_status = 0; // Status of transmission
 
 volatile unsigned int time_out_timer;
-volatile byte input_rx_buffer;                           // Temp input buffer from CMX589, 8 bit buffer is filled by RX_589.
-volatile byte input_rx_buffer_bit_counter = 0;           // When 8 bits are received byte full wil be placed in radio_rx_buffer,
-volatile int radio_rx_buffer_counter = radio_rx_buf_len; // pointer for end of buffer indication
-volatile byte radio_tx_buffer[radio_tx_buf_len + 1];     // ringbuffer for datastream from CMX589
-volatile int radio_tx_buffer_in_counter = 0;             // pointer for end of buffer indication
-volatile int radio_tx_buffer_out_counter = 0;
-volatile int radio_tx_buffer_bit_counter = 0;
-volatile int radio_tx_buffer_gen_counter = 0;
-volatile boolean rx_int_flag = false;
+volatile byte input_rx_buffer;                       // Temp input buffer from CMX589, 8 bit buffer is filled by RX_589.
+volatile byte input_rx_buffer_bit_counter = 0;       // When 8 bits are received byte full wil be placed in ais_rx_buffer,
+volatile int ais_rx_buffer_counter = ais_rx_buf_len; // pointer for end of buffer indication
 
-volatile byte temp_shift_buffer[23];                            // Fifo buffer for data analisys. 21 X 8 bits can be shift right.
-volatile int temp_shift_buffer_counter = radio_rx_buf_len - 50; // When 8 bits are shifted a new byte pointed by counter will be
-volatile byte temp_shift_buffer_bit_counter = 0;                // placed in temp_shift_buffer[0].
+// buffer: <8 bit ramp up><24 bit preamble><8 bit start flag><168 bit payload><16 bit CRC><8 bit stop flag><24 bit buffer>
+volatile byte ais_rx_buffer[ais_rx_buf_len + 1];
+
+volatile boolean rx_int_flag = false;
+volatile boolean six_bit_ready = false;
+volatile boolean eight_bit_ready = false;
 
 unsigned long vhfMillis = 0;
 bool scan = false;
@@ -67,22 +61,29 @@ void setup()
 
   Serial.println("");
   Serial.println("Searching...");
+  for (size_t i = 0; i < sizeof(ais_rx_buf_len); i++)
+  {
+    ais_rx_buffer[i] = 0;
+  }
 }
 
 //========================================= Wait for 24 bit Preamble ===================================================
 void Wait_for_preamble()
 {
-  if (temp_shift_buffer[2] == B10101010 && temp_shift_buffer[1] == B10101010 && temp_shift_buffer[0] == B10101010)
+  if ((ais_rx_buffer[ais_rx_buf_len - 2] == B01010101 && ais_rx_buffer[ais_rx_buf_len - 1] == B01010101 && ais_rx_buffer[ais_rx_buf_len] == B01010101) ||
+      (ais_rx_buffer[ais_rx_buf_len - 2] == B10101010 && ais_rx_buffer[ais_rx_buf_len - 1] == B10101010 && ais_rx_buffer[ais_rx_buf_len] == B10101010))
   {
     rx_status = 1;
-    Serial.println("Preamble found!");
+    Serial.println(millis());
+    Serial.println(" - Preamble found!");
+    input_rx_buffer_bit_counter = 0;
   }
 }
 
 //========================================= Wait for Start flag ===================================================
 void Wait_for_start_flag()
 {
-  if (temp_shift_buffer[0] == B01111110)
+  if (ais_rx_buffer[ais_rx_buf_len] == B01111110)
   {
     rx_status = 2;
     Serial.println("Start flag found!");
@@ -92,58 +93,58 @@ void Wait_for_start_flag()
 //========================================= Wait for Stop flag ===================================================
 void Wait_for_stop_flag()
 {
-  if (temp_shift_buffer[0] == B01111110)
+  if (ais_rx_buffer[ais_rx_buf_len] == B01111110)
   {
     rx_status = 0;
     Serial.println("Stop flag found!!");
+    for (int i = 0; i < sizeof(ais_rx_buffer); i++)
+      PRINTBINL(ais_rx_buffer[i]);
+    Serial.println();
   }
 }
 
 //============================ Rotate temp_shift_buffer ===========================================================
 void RX_buffer_shift()
 {
-  temp_shift_buffer_bit_counter += 1; // Increment curent possition
-
-  if (temp_shift_buffer_bit_counter > 7) // Check if byte is full
+  size_t size = sizeof(ais_rx_buffer);
+  for (size_t i = 0; i < size; i++)
   {
-    temp_shift_buffer_bit_counter = 0;                                 // If full clear bit counter
-    temp_shift_buffer[0] = radio_tx_buffer[temp_shift_buffer_counter]; //!       // Read byte from ringbuffer
-    temp_shift_buffer_counter -= 1;                                    // Relocate ringbuffer
-    if (temp_shift_buffer_counter == 0)
-      temp_shift_buffer_counter = radio_rx_buf_len; // Reloop ringbuffer
-  }
-  // RX shift buffer
-  for (int RX_buffer_shift_counter = 22; RX_buffer_shift_counter >= 0; RX_buffer_shift_counter--)
-  {
-    temp_shift_buffer[RX_buffer_shift_counter] = temp_shift_buffer[RX_buffer_shift_counter] >> 1;
-    bitWrite(temp_shift_buffer[RX_buffer_shift_counter], 7, bitRead(temp_shift_buffer[RX_buffer_shift_counter - 1], 0));
+    // if (ais_rx_buffer[i] < 0x10)
+    //   Serial.print('0');
+    // Serial.print(ais_rx_buffer[i], HEX);
+    // PRINTBINL(ais_rx_buffer[i]);
+    ais_rx_buffer[i] = ais_rx_buffer[i] << 1;
+    if (i < size - 1)
+      bitWrite(ais_rx_buffer[i], 0, bitRead(ais_rx_buffer[i + 1], 7));
   }
 }
 //========================================= Interrupt routine RX clk from CMX589 =================================
 void RX_CMX589()
 {
-
   time_out_timer += 1;
-  bitWrite(input_rx_buffer, 7, digitalRead(pinRXD_U)); // Read RX data pin normal
+  bitWrite(input_rx_buffer, 0, digitalRead(pinRXD_U)); // Read RX data pin normal
   input_rx_buffer_bit_counter += 1;                    // Increment bit counter
+  ais_rx_buffer[ais_rx_buf_len] = input_rx_buffer;     //!     // Copy byte to ringbuffer
+  input_rx_buffer = input_rx_buffer << 1;              // Rotate input buffer
 
-  if (input_rx_buffer_bit_counter == 8) // If byte is full copy byte to ringbuffer
+  if (rx_status == 2 && input_rx_buffer_bit_counter == 6) // Start collecting 6 bit data stream.
   {
-    // PRINTBIN(input_rx_buffer);
-    // Serial.print(" - ");
-    // Serial.println(input_rx_buffer, HEX);
-
-    radio_tx_buffer[radio_rx_buffer_counter] = input_rx_buffer; //!     // Copy byte to ringbuffer
-    radio_rx_buffer_counter -= 1;                               // Rotate ringbuffer
-    if (radio_rx_buffer_counter == 0)
-      radio_rx_buffer_counter = radio_rx_buf_len; // Loop buffer around if needed
-    input_rx_buffer_bit_counter = 0;              //
+    // ais_rx_buffer[ais_rx_buf_len] = input_rx_buffer; //!     // Copy byte to ringbuffer
+    // input_rx_buffer = 0;
+    six_bit_ready = true;
+    input_rx_buffer_bit_counter = 0; //
+  }
+  else if (input_rx_buffer_bit_counter == 8) // If byte is full copy byte to ringbuffer
+  {
+    // ais_rx_buffer[ais_rx_buf_len] = input_rx_buffer; //!     // Copy byte to ringbuffer
+    // input_rx_buffer = 0;
+    eight_bit_ready = true;
+    input_rx_buffer_bit_counter = 0; //
   }
   else
-  {                                         // If byte is not full
-    input_rx_buffer = input_rx_buffer >> 1; // Rotate input buffer
+  { // If byte is not full
+    // input_rx_buffer = input_rx_buffer << 1; // Rotate input buffer
   }
-
   rx_int_flag = true; // Set rx_int_flag for general purposes.
 }
 
@@ -161,18 +162,33 @@ void loop()
 
   if (rx_int_flag == true)
   {
-    RX_buffer_shift(); // Rotate RX buffer
-
+    RX_buffer_shift();
     // Wait for preamble
     if ((rx_status == 0) && (rx_int_flag == true))
+    {
       Wait_for_preamble();
-
+    }
     // Wait for frame sync
-    if ((rx_status == 1) && (rx_int_flag == true))
+    else if ((rx_status == 1) && (rx_int_flag == true))
+    {
       Wait_for_start_flag();
-
-    if ((rx_status == 2) && (rx_int_flag == true))
+    }
+    else if ((rx_status == 2) && (rx_int_flag == true))
+    {
       Wait_for_stop_flag();
+    }
+    if (eight_bit_ready == true)
+    {
+      // size_t size = sizeof(ais_rx_buffer);
+      // for (size_t i = 0; i < size; i++)
+      // {
+      //   eight_bit_ready = false;
+      //   if (ais_rx_buffer[i] < 0x10)
+      //     Serial.print('0');
+      //   Serial.print(ais_rx_buffer[i], HEX);
+      // }
+      // Serial.println();
+    }
   }
 
   rx_int_flag = false;
